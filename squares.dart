a659cc988ca405326ee3f1ea93d312b51290c9e0
BriouaNorElHouda <84919515+BriouaNorElHouda@users.noreply.github.com>
2022-01-31 17:15:18 +0100

    Create squares.dart

---
+++ squares.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(Home());
+}
+
+class Home extends StatelessWidget{
+
+  Widget build(context){
+
+    return MaterialApp(
+      home: Scaffold (
+
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            Row(
+
+
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(40.0),
+                  color: Colors.blue,
+                  child: Text('-1-'),
+                ),
+                SizedBox(height:70),
+                Container(
+                  padding: EdgeInsets.all(40.0),
+                  color: Colors.green,
+                  child: Text('-2-'),
+
+                ),
+              ],
+            ),
+
+            Row(
+              children: <Widget>[
+
+                Container(
+                  padding: EdgeInsets.all(40.0),
+                  color: Colors.purple,
+                  child: Text('-3-'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(40.0),
+                  color: Colors.yellow,
+                  child: Text('-4-'),
+                ),
+              ],
+
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text ("click"),
+          backgroundColor: Colors.blue, onPressed: () {  },
+        ),
+      ),
+
+    );
+  }
+
+}
